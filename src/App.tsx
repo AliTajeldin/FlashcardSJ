00f14d831c7290ef7cc9ca26a17cb93f125c25bc
Ali Tajeldin <alitajeldin@gmail.com>
2022-12-03 22:09:24 -0800

    initial commit

---
+++ src/App.tsx
@@ -0,0 +1,23 @@
+import type { Component } from 'solid-js';
+
+import styles from './App.module.css';
+
+const App: Component = () => {
+  return (
+    <div class={styles.App}>
+      <div>
+      <p>
+          Hello World!
+        </p>
+        <p>
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Enim facilisis gravida neque convallis a cras. Montes nascetur ridiculus mus mauris vitae ultricies leo. Risus nullam eget felis eget nunc. Et malesuada fames ac turpis egestas maecenas. Phasellus faucibus scelerisque eleifend donec pretium vulputate. Ullamcorper sit amet risus nullam eget felis eget. Enim facilisis gravida neque convallis a. Pellentesque habitant morbi tristique senectus. Proin libero nunc consequat interdum. A erat nam at lectus urna.
+        </p>
+        <p>
+          Laoreet non curabitur gravida arcu ac tortor dignissim convallis. Vitae purus faucibus ornare suspendisse sed nisi lacus sed. Volutpat maecenas volutpat blandit aliquam etiam erat. Duis at tellus at urna condimentum. Sit amet justo donec enim diam vulputate ut pharetra. In massa tempor nec feugiat. Lacinia quis vel eros donec. Sed vulputate odio ut enim blandit. Amet nulla facilisi morbi tempus iaculis. Elementum integer enim neque volutpat ac tincidunt vitae. Venenatis lectus magna fringilla urna porttitor. Blandit libero volutpat sed cras ornare. Amet commodo nulla facilisi nullam vehicula ipsum. Dictum at tempor commodo ullamcorper. Hendrerit gravida rutrum quisque non tellus orci ac.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default App;
