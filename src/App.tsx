07eb73eec26f68a47bc8711f4ab8dbb0c4e67da0
Ali Tajeldin <alitajeldin@gmail.com>
2022-12-04 12:22:01 -0800

    yes/no buttons

--- src/App.tsx
+++ src/App.tsx
@@ -3,20 +3,28 @@ import { Component, createSignal } from 'solid-js';
 import styles from './App.module.css';
 import { Card, CardMgr } from './cards';
 
+//  ✓ ✔︎ ✕ ✖︎ 
+
 const App: Component = () => {
   const cardMgr = new CardMgr();
   const [ card, setCard ] = createSignal<Card>(cardMgr.getNextCard());
+  const [ showAnswer, setShowAnswer ] = createSignal(false);
 
-  const nextCard = () => {
-    setCard(cardMgr.getNextCard())
+  const answerYes = () => {
+    if (showAnswer()) {
+      setCard(cardMgr.getNextCard())
+    }
+    setShowAnswer(!showAnswer());
   }
+
   return (
     <div class={styles.App}>
       <div class={styles.Narrow}>
         <div class={styles.Item}>{card().item1}</div>
-        <div class={styles.Item}>{card().item2}</div>
-        <div class={styles.Answer}>
-          <button onClick={nextCard}>Button</button>
+        <div class={styles.Item}>{showAnswer() ? card().item2 : ""}</div>
+        <div class={styles.Answers}>
+          <button class={styles.AnswerNo} disabled={!showAnswer()} onClick={answerYes}>✖︎</button>
+          <button class={styles.AnswerYes} onClick={answerYes}>✔︎</button>
         </div>
         
       </div>
