449db3a6db8d1d3a112c82b178018cd248416480
Ali Tajeldin <alitajeldin@gmail.com>
2022-12-07 18:40:54 -0800

    code cleanup

--- src/App.tsx
+++ src/App.tsx
@@ -41,20 +41,46 @@ const App: Component = () => {
     </div>
   }
 
+  const Header: Component = () => {
+    return (
+      <div class={styles.Header}>
+        <select name="cardset" id="cardset" class={styles.CardSetSelect}>
+          <option value="volvo">Volvo</option>
+          <option value="saab">Saab</option>
+          <option value="opel">Opel</option>
+          <option value="audi">Audi</option>
+        </select>
+      </div>
+    );
+  }
+
+  const Footer: Component = () => {
+    return (
+      <div class={styles.Answers}>
+        <button class={styles.AnswerOpt} onClick={() => setShowOptions(!showOptions())}>⚙</button>
+        <button class={styles.AnswerNo} disabled={!showAnswer()} onClick={answerYes}>✖︎</button>
+        <button class={styles.AnswerYes} onClick={answerYes}>✔︎</button>
+      </div>
+    );
+  }
+
+  const OptionsModal: Component = () => {
+    return (
+      <Show when={showOptions()}>
+        <Options onExit={() => setShowOptions(false)} />
+      </Show>
+    );
+  }
+
   return (
     <div class={styles.App}>
       <div class={styles.Narrow}>
         <Show when={card()} fallback={<div>Loading...</div>}>
+          <Header />
           <Item label={card()!.item1Label} value={card()!.item1} show={true} />
           <Item label={card()!.item2Label} value={card()!.item2} show={showAnswer()} />
-          <div class={styles.Answers}>
-            <button class={styles.AnswerOpt} onClick={() => setShowOptions(!showOptions())}>⚙</button>
-            <button class={styles.AnswerNo} disabled={!showAnswer()} onClick={answerYes}>✖︎</button>
-            <button class={styles.AnswerYes} onClick={answerYes}>✔︎</button>
-          </div>
-          <Show when={showOptions()}>
-            <Options onExit={() => setShowOptions(false)} />
-          </Show>
+          <Footer />
+          <OptionsModal />
         </Show>
       </div>
     </div>
